26f9033662dc649ad22479de01a6812660705176
Durant Schoon <durantschoon@users.noreply.github.com>
2025-08-16 22:14:41 -0400

    Create 06-postinstall-own-terminal.sh

---
+++ 06-postinstall-own-terminal.sh
@@ -0,0 +1,14 @@
+mkdir -p /mnt/etc/guix
+cat > /mnt/etc/guix/channels.scm <<'EOF'
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix.git")
+        (branch "master")))
+EOF
+
+guix pull -C /mnt/etc/guix/channels.scm
+guix system reconfigure
