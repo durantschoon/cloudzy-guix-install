2d5203a4cea50a9bcde02d6e22d44cd04f119adf
Durant Schoon <durantschoon@users.noreply.github.com>
2025-08-16 22:10:24 -0400

    Create 04-system-init.sh

---
+++ 04-system-init.sh
@@ -0,0 +1,24 @@
+export TMPDIR=/mnt/var/tmp
+mkdir -p "$TMPDIR" && chmod 1777 "$TMPDIR"
+rm -rf /var/guix/substitute-cache/* 2>/dev/null
+
+fallocate -l 4G /mnt/swapfile || dd if=/dev/zero of=/mnt/swapfile bs=1M count=4096
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
+
+# Verify:
+swapon --show
+free -h
+
+export GIT_HTTP_MAX_REQUESTS=2
+export GIT_HTTP_LOW_SPEED_LIMIT=1000
+export GIT_HTTP_LOW_SPEED_TIME=60
+
+guix pull --commit=v1.4.0
+
+guix system init /mnt/etc/config.scm /mnt
+
+sync
+umount -R /mnt
+reboot
